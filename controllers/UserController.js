14e14f779db9e685b573b556fedd918d53a866e0
Domantas <domantas.malukas@gmail.com>
2023-04-17 21:46:48 +0300

    04-17

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -56,7 +56,7 @@ var jwt = require('jsonwebtoken');
            "id": results[0].user_id
         }
 
-        let jwtToken = jwt.sign(rez, process.env.TOKEN_SECRET, { expiresIn: '1h' })
+        let jwtToken = jwt.sign(rez, process.env.TOKEN_SECRET, { expiresIn: '10s' })
         res.json({token: jwtToken, blocked:  results[0].blocked });
       }
       else{
